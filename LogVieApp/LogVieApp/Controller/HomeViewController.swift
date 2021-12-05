c8982dddbc4199c68266228b8ddc650214c25f7e
Kim Seohyun <cestbonciel88@knou.ac.kr>
2021-12-05 18:30:40 +0900

    Login view code change for Home View

--- LogVieApp/LogVieApp/Controller/HomeViewController.swift
+++ LogVieApp/LogVieApp/Controller/HomeViewController.swift
@@ -13,14 +13,21 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
     }
     // 로그인 정보 유무 ( 로그인 유지 환경) 
-    override func viewDidAppear(_ animated: Bool) {        
+    override func viewDidAppear(_ animated: Bool) {
         if UserDefaults.standard.string(forKey: "nickname") == nil{
-            if let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "Login") as? LoginViewController {
+            if let loginVC = UIStoryboard(name:"Home",bundle: nil).instantiateViewController(withIdentifier: "Login") as? LoginViewController {
                 loginVC.modalPresentationStyle = .fullScreen
                 self.present(loginVC, animated: true)
             }
             self.tabBarController?.selectedIndex = 0
         }
+        /*if UserDefaults.standard.string(forKey: "nickname") == nil{
+            if let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "Login") as? LoginViewController {
+                loginVC.modalPresentationStyle = .fullScreen
+                self.present(loginVC, animated: true)
+            }
+            self.tabBarController?.selectedIndex = 0
+        }*/
     }
     
     /*
